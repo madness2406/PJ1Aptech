a8c91cdefb30c1b16834c391a6b74382fbf2b99f
Nguyễn Trần Tuân <tuandb2406@gmail.com>
2022-08-27 20:03:19 +0700

    update code

---
+++ AptechPJSEM2/src/Process/StudentManager.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Process;
+
+import Database.DBConnection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author phamtrunghieu
+ */
+public class StudentManager {
+
+    public static boolean StudentToTable(JTable jTable) {
+        try {
+            DefaultTableModel dfTableModel = (DefaultTableModel) jTable.getModel();
+            dfTableModel.setRowCount(0);
+            DBConnection db = new DBConnection();
+            ResultSet rs = db.GetData("Select Id, Name, Gender, DOB, POB, PhoneNo From student");
+            String[] row = new String[4];
+            while (rs.next()) {
+                row[0] = rs.getString(1);
+                row[1] = rs.getString(2);
+                row[2] = rs.getString(3);
+                row[3] = rs.getString(4);
+                dfTableModel.addRow(row);
+            }
+            return true;
+        } catch (SQLException ex) {
+            java.util.logging.Logger.getLogger(DBConnection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public static boolean Add(String name, String gender, String dob, String pob, String phoneNo) {
+        DBConnection dbconn = new DBConnection();
+        String sql;
+            sql = "Insert Into student (Name,Gender,DOB,POB,PhoneNo)"
+                    + " values (N'" + name + "',N'" + gender + "',N'" + dob + "',N'"
+                    + pob + "',N'" + phoneNo +"')";
+        return dbconn.UpdateData(sql);
+
+    }
+
+}
